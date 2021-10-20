4fe4bb3c5e502e32b56bc9bd9f698849bf0182bb
armin bakhshi <arminbkh0921@yahoo.com>
2021-10-21 00:50:23 +0330

    create reducer

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const BUY_CAKE = "BUY_CAKE";
 
 //action creator
 const buyCake = () => {
-    return { type: BUY_CAKE };
-} 
+  return { type: BUY_CAKE };
+};
 //action creator
+
+const initialState = {
+  numOfCake: 10,
+  shampoo: 15,
+};
+
+//reducer
+const reducer = (state = initialState, action) => {
+  switch (key) {
+    case BUY_CAKE: {
+      return { ...state, numOfCake: state.numOfCake - 1 };
+    }
+    default:
+      return state;
+  }
+};
+//reducer
