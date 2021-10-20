7bbf53d41f52d96384092046962a5bc2ae3ba242
armin bakhshi <arminbkh0921@yahoo.com>
2021-10-21 01:45:49 +0330

    import redux and create a store and called dispatch and getState and subscribe methods

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const redux = require("redux");
+
+const createStore = redux.createStore;
+
 const BUY_CAKE = "BUY_CAKE";
 
 //action creator
@@ -13,7 +17,7 @@ const initialState = {
 
 //reducer
 const reducer = (state = initialState, action) => {
-  switch (key) {
+  switch (action.type) {
     case BUY_CAKE: {
       return { ...state, numOfCake: state.numOfCake - 1 };
     }
@@ -22,3 +26,11 @@ const reducer = (state = initialState, action) => {
   }
 };
 //reducer
+
+const store = createStore(reducer);
+console.log("initial state: ", store.getState());
+const unsubscribe = store.subscribe(() => console.log("updated state: ", store.getState()));
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();
